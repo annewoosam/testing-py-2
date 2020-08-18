f0fa4336e094d1dce3065136a9117a97f1b48c92
vagrant <vagrant@vagrant.vm>
2020-08-18 19:21:21 +0000

    resolution was uncommenting test.py items which create and connect to db then adding ref to route and assertion

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+
 class Game(db.Model):
     """Board game."""
 
@@ -12,20 +13,23 @@ class Game(db.Model):
     description = db.Column(db.String(100))
 
 
+def example_data():
+    game = Game(name="Power Grid",
+                description="supply the most cities with power")
+    db.session.add(game)
+    db.session.commit()
+
+
 def connect_to_db(app, db_uri="postgresql:///games"):
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     db.app = app
     db.init_app(app)
 
 
-def example_data():
-    """Create example data for the test database."""
-    # FIXME: write a function that creates a game and adds it to the database.
-    print("FIXME")
-
-
 if __name__ == '__main__':
     from party import app
-
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     connect_to_db(app)
     print("Connected to DB.")
+
+    
